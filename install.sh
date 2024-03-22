b350856347f8ad135ae4c7d4a71a51728e040c1d
Benjamin Bingham <ben@benbingham.ca>
2024-03-22 12:29:37 -0600

    Switch to install over cp

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp vertex-data-mime.xml /usr/share/mime/packages
+install vertex-data-mime.xml /usr/share/mime/packages/
 update-mime-database /usr/share/mime
 
-cp org.kde.okular-vertex-data.metainfo.xml /usr/share/metainfo
-cp okularApplication_vertex_data.desktop /usr/share/applications
+install org.kde.okular-vertex-data.metainfo.xml /usr/share/metainfo/
+install okularApplication_vertex_data.desktop /usr/share/applications/
 
-cp okularGenerator_vertex_data.so /usr/lib/x86_64-linux-gnu/qt6/plugins/okular_generators
+install okularGenerator_vertex_data.so /usr/lib/x86_64-linux-gnu/qt6/plugins/okular_generators/
