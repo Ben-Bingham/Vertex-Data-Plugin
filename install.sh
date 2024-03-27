71a789acb8ed1ca020569f8b33d07d0d07d3de59
Benjamin Bingham <ben@benbingham.ca>
2024-03-26 22:31:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ update-mime-database /usr/share/mime
 install org.kde.okular-vertex-data.metainfo.xml /usr/share/metainfo/
 install okularApplication_vertex_data.desktop /usr/share/applications/
 
+install okularVertexData.desktop /usr/share/kservices5/
+
 install okularGenerator_vertex_data.so /usr/lib64/qt5/plugins/okular/generators
